48f199c300b642a5bbdf024a6b6882276a655265
Kelen Y. Yafuso <kyyafuso@gmail.com>
2019-08-24 14:40:02 -0700

    Doesn't work yet

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,12 +1,64 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+function calculate(balance, rate, term){
+  return html;
+  }
 
 export default class App extends React.Component {
   // your Javascript goes here
   render() {
     return (
       <div className='container'>
-        {/* your JSX goes here */}
+        {
+          <div><input name='balance' type='number'></input>
+          <input name='rate' type='number'></input>
+          <select name='term'>
+            <option value='15, 30'>15</option>
+            <option value='30'>30</option>
+            </select>
+            <button name='submit' onclick = 'calculate(.state)'>Submit!</button>
+            <div name='output' id='output'></div>
+      }
       </div>
-    );
+        });
+  </div>
+  ,
+  constructor(props){
+    super(props);
+    this.state = {}
+    console.log(this.props);
+    // I do not understand what a constructor does after three hours of reading about it. LIAM HELP ME NIISAN
+    //more arbitrary notes to fill th epage while i read nonsense this is more stuffmore fluff ill figure out a way to get us out of here
   }
+    //initialize state of app with constructor function
+  
+               /*insert function to update state values when an input changes, 
+               using event binding. Once this is in place, return to your HTML 
+               and add an onChange event to each of the input elements that calls this new method.*/
+
+               /*calculate function to determine the mortgage payment and then set state to bind the 
+               result to the div with an id of 'output' as a string like this: $1945.09 is your payment.*/
+
+
+               /*This function should accept 3 parameters: balance, rate, and term. Finally, return to your 
+               HTML and add an onClick event to this button that calls the calculate function while passing 
+               the state object as an argument.*/
+               /**THIS IS A BUNCH OF RANDOM CHARACTERS SO WAKITIME WILL DO SOMETHING FOR ME THANKS 
+                * HERES SOME MORE RANDOM CHARACTERS FOR YOU WAKATIME HAVE FUN WITH THAT
+                * HEY LOOK AT THAT MORE WHITE TEXT WAKATIME
+                * k more textmore text more textmoretext SHUT UP CORTANA
+                * My legs are tingling. I can feel my heart beating. This is probably a good thing because it means it has more strength, more pressure
+                * I cannnot sit still
+                * time time time time time time time time time time time time 
+                */
+
 }
+
+/*
+class helloWorld{
+public static void main(string[] args){
+  system.out.println("Hello World!")
+}} This is java*/
+
+
+/** Need to research options menus once again. Look up how to generate them using constructors, review constructor structure */
